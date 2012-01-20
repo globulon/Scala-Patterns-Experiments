3e4a1561acd36e9674fd0de2119400203c876414
Marc-Daniel Ortega <globulon@gmail.com>
2012-01-20 22:12:15 +0100

    Finalized samples

---
+++ src/main/scala/com/promindis/Logger/StringWriter.scala
@@ -0,0 +1,13 @@
+package com.promindis.Logger
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: omadas
+ * Date: 09/01/12
+ * Time: 13:56
+ * To change this template use File | Settings | File Templates.
+ */
+
+class StringWriter {
+
+}
---
+++ src/main/scala/com/promindis/user/UseState.scala
@@ -0,0 +1,13 @@
+package com.promindis.user
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: omadas
+ * Date: 15/01/12
+ * Time: 17:20
+ * To change this template use File | Settings | File Templates.
+ */
+
+class UseState {
+
+}
