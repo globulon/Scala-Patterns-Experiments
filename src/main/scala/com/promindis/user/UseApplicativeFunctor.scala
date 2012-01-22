55f894c2672b54d6007ad239eadb317b2c0ddc28
Marc-Daniel Ortega <globulon@gmail.com>
2012-01-22 21:36:32 +0100

    update ramblings on applicative functors

--- src/main/scala/com/promindis/user/UseApplicativeFunctor.scala
+++ src/main/scala/com/promindis/user/UseApplicativeFunctor.scala
@@ -4,21 +4,21 @@ import com.promindis.patterns.{ApplicativeBuilder, Applicative}
 
 
 object UseApplicativeFunctor {
-  implicit object ApplicativeListBuilder extends ApplicativeBuilder[List]{
+  implicit object ApplicativeListBuilder extends ApplicativeBuilder[Iterable]{
 
-    implicit def apply[T](list : List[T]) = new Applicative[T, List] {
-      override def map[U](f: (T) => U) = list.map(f)
+    implicit def apply[T](iterable : Iterable[T]) = new Applicative[T, Iterable] {
+      override def map[U](f: (T) => U) = iterable.map(f)
     }
 
-    def flatten[T](m: List[List[T]]) = m.flatten
+    def flatten[T](m: Iterable[Iterable[T]]) = m.flatten
   }
 
 
 
-  import Applicative._
   import ApplicativeListBuilder._
 
   def main(args: Array[String]) {
+
     val f: Int => Int = _ * 10
     val g: Int => Int = _ + 100
     val h: Int => Int = _ ^ 2
@@ -26,14 +26,16 @@ object UseApplicativeFunctor {
     println(List(f, g, h) :*: List(1, 2, 3))
 
     def add(a: Int)(b: Int): Int = a + b
-    def uadd = Function.uncurried(add _)
+//    def uadd = Function.uncurried(add _)
     def mul(a: Int)(b: Int): Int = a * b
-    //    val j: Int => Int => Int = _ * _
+
     println((List(add _, mul _) :*: List(1, 2)) :*: List(3, 4))
 
     println(f :@: List(1, 2))
 
     println((add _ :@: List(1, 2)):*: List(3,4))
-    println(liftA2(uadd, List(1, 2), List(3,4)))
+
+    println((add _ :@: Set(5, 6)):*: Set(7, 8))
+//    println(liftA2(uadd, List(1, 2), List(3,4)))
   }
 }
