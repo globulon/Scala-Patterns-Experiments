2e20c13dd7b230fc0be64dcca4489459d17f9bbb
Marc-Daniel Ortega <globulon@gmail.com>
2012-02-06 20:15:23 +0100

    gain cosmetic

---
+++ src/main/scala/com/promindis/user/Futures.scala
@@ -0,0 +1,49 @@
+package com.promindis.user
+
+import com.promindis.patterns.{Functor, Monad, Applicative}
+import java.util.concurrent.{TimeUnit, Future, Callable}
+import Applicative._
+
+/**
+ * Date: 06/02/12
+ * Time: 20:02
+ */
+
+case class W(value: Int) extends Callable[Int] {
+  def call() = {
+    Thread.sleep(3000)
+    value
+  }
+}
+
+object Futures {
+  implicit object ApplicativeFuture extends Applicative[Future] with Monad[Future] with Functor[Future] {
+    def apply[T](data: T) = new Future[T] {
+      override def get() = data
+
+      override def get(timeout: Long, unit: TimeUnit) = get()
+
+      override def cancel(mayInterruptIfRunning: Boolean) = false
+
+      override def isCancelled = false
+
+      override def isDone = true
+    }
+
+    def flatten[T](m: Future[Future[T]]) = m.get()
+
+    def map[T, U](source: Future[T])(f: (T) => U) = new Future[U] {
+      override def get() = f(source.get())
+
+      override def get(timeout: Long, unit: TimeUnit) = f(source.get(timeout, unit))
+
+      override def cancel(mayInterruptIfRunning: Boolean) = false
+
+      override def isCancelled = false
+
+      override def isDone = true
+    }
+  }
+
+
+}
--- src/main/scala/com/promindis/user/UseApplicativeFunctor.scala
+++ src/main/scala/com/promindis/user/UseApplicativeFunctor.scala
@@ -2,44 +2,12 @@ package com.promindis.user
 
 import com.promindis.patterns._
 import java.util.concurrent._
+import Applicative._
+import Futures._
 
-
-case class W(value: Int) extends Callable[Int] {
-  def call() = {
-    Thread.sleep(3000)
-    value
-  }
-}
 
 object UseApplicativeFunctor {
 
-  implicit object ApplicativeFuture extends Applicative[Future] with Monad[Future] with Functor[Future] {
-    def apply[T](data: T) = new Future[T] {
-      override def get() = data
-
-      override def get(timeout: Long, unit: TimeUnit) = get()
-
-      override def cancel(mayInterruptIfRunning: Boolean) = false
-
-      override def isCancelled = false
-
-      override def isDone = true
-    }
-
-    def flatten[T](m: Future[Future[T]]) = m.get()
-
-    def map[T, U](source: Future[T])(f: (T) => U) = new Future[U] {
-      override def get() = f(source.get())
-
-      override def get(timeout: Long, unit: TimeUnit) = f(source.get(timeout, unit))
-
-      override def cancel(mayInterruptIfRunning: Boolean) = false
-
-      override def isCancelled = false
-
-      override def isDone = true
-    }
-  }
 
   def mul = (a: Int) => a * 10
   def add = (a: Int, b: Int) => a + b
@@ -50,26 +18,45 @@ object UseApplicativeFunctor {
   }
 
 
-  def main(args: Array[String]) {
-    import Applicative._
-
+  def basics() {
     println(liftA2(add, List(1, 2), List(3, 4)))
     println(liftA3(addd, List(1, 2), List(3, 4), List(5, 6)))
     println(add :@: List(1, 2) :*: List(3, 4))
     println(addd :@: List(1, 2) :*: List(3, 4) :*: List(5, 6))
+  }
 
-    val list: List[Option[Int]] = List(Some(1), Some(2), Some(3))
-    println(list.sequenceA)
-
+  def futures() {
     implicit val pool = Executors.newCachedThreadPool()
 
     val start = System.currentTimeMillis
 
-    val result = add:@:exec(W(1), W(2), W(3)):*:exec(W(4), W(5), W(6))
+    val result = add :@: exec(W(1), W(2), W(3)) :*: exec(W(4), W(5), W(6))
 
     val ellapsed = System.currentTimeMillis - start
     println("Got " + result + " in " + ellapsed + " ms")
 
     pool.shutdown()
+  }
+
+  def getConnection(input: List[Option[String]]) = {
+      def doSomething(fromParameters: List[String]) =  fromParameters.toString()
+
+      for {
+        withParams <- input.sequenceA
+      } yield doSomething(withParams)
+  }
+
+  def main(args: Array[String]) {
+
+    basics()
+
+    val list: List[Option[Int]] = List(Some(1), Some(2), Some(3))
+    println(list.sequenceA)
+
+    println(getConnection(List(Some("url"), Some("user"), Some("passwd"))))
+    println(getConnection(List(Some("url"), None, Some("passwd"))))
+
+
+    futures()
   }
 }
