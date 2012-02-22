e539f1c0f28499c220ec423c2b4795122ebcf83b
Marc-Daniel Ortega <globulon@gmail.com>
2012-02-22 20:46:01 +0100

    Got a first example for reader monad

--- src/main/scala/com/promindis/user/ReaderMonad.scala
+++ src/main/scala/com/promindis/user/ReaderMonad.scala
@@ -6,14 +6,32 @@ import com.promindis.patterns._
  */
 
 object ReaderMonad {
-
   def `*2` = (x: Int) => x * 2
   def `+10` = (x: Int) => 10 + x
+
+  def extract[String](name: String)(fromMap: Map[String, String]) =
+    fromMap.get(name)
 
   def main(args: Array[String]) {
     println((for {
       a <- `*2`
       b <-  `+10`
     } yield (a + b))(3))
+
+    val fittingProperties = Map (
+      "login" → "thumper",
+      "password" → "thumpthump",
+      "url" → "jdbc:forest"
+    )
+
+    def configBuilder[T, M[_, _]](extract: String ⇒ M[String, T] ⇒ Option[T]) = for {
+      user ← extract("login")_
+      password ← extract("password")_
+      url ← extract("url")_
+    } yield (List(user, password, url))
+
+    val config = configBuilder(extract)(fittingProperties)
+    println(config.sequenceA)
+
   }
 }
